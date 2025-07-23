8b9fc0b1c536b4fd3699724036802ea8fe6298dd
IAmMarcelJung <MarcelJung97@web.de>
2025-07-23 17:45:24 +0200

    feat: add function to load a JSON file

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import ujson
 
 
 def file_exists(path):
@@ -9,3 +10,16 @@ def file_exists(path):
         return True
     except OSError:
         return False
+
+
+def load_config(config_file: str):
+    """Load a JSON configuration file
+
+
+    Parameters
+    ----------
+    conig_file : str
+        The path to the configuration file.
+    """
+    with open(config_file) as f:
+        return ujson.load(f)
