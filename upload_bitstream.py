718d97b0bb7230607e0f7ee7ce4451b1b36ec1ae
IAmMarcelJung <MarcelJung97@web.de>
2025-07-23 17:47:00 +0200

    feat: load the name of the bistream file from the JSON config file

--- upload_bitstream.py
+++ upload_bitstream.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
-
 from modules.board import Board
+from modules.helpers import load_config, file_exists
 
 
 def run():
+    config_file = "config.json"
+    if not file_exists(config_file):
+        print(f"Configuration file '{config_file}' not found. Please create it.")
+        return
+
+    config = load_config(config_file)
+
     board = Board(
         fpga_sclk=17,
         fpga_sdata=18,
     )
 
-    board.transmit_bitstream("bitstream.bin")
+    bitstream_file = config.get("bitstream_file")
+
+    if not bitstream_file:
+        print("Missing 'bitstream_file' in config.")
+        return
+
+    board.transmit_bitstream(bitstream_file)
     board.disable_bitbang()
