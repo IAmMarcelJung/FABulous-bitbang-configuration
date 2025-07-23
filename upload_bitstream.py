2dd483e17432b3ffd1766cf5c2eac06a99ec084a
IAmMarcelJung <MarcelJung97@web.de>
2025-07-23 19:07:05 +0200

    feat: add s_clk and s_data as configuration parameters and use the configuration validation function-madd more constants for fixed strings

--- upload_bitstream.py
+++ upload_bitstream.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 
 from modules.board import Board
-from modules.helpers import load_config, file_exists
+from modules.helpers import load_config, file_exists, validate_config
+
+
+KEY_BITSTREAM_FILE = "bitstream_file"
+KEY_FPGA_SCLK = "fpga_sclk"
+KEY_FPGA_SDATA = "fpga_sdata"
+
+CONFIG_SCHEMA = {
+    KEY_BITSTREAM_FILE: str,
+    KEY_FPGA_SCLK: int,
+    KEY_FPGA_SDATA: int,
+}
+
+CONFIG_FILE = "config.json"
 
 
 def run():
-    config_file = "config.json"
-    if not file_exists(config_file):
-        print(f"Configuration file '{config_file}' not found. Please create it.")
+    if not file_exists(CONFIG_FILE):
+        print(f"Configuration file '{CONFIG_FILE}' not found. Please create it.")
         return
+    try:
+        config = load_config(CONFIG_FILE)
+        validate_config(config, CONFIG_SCHEMA)
 
-    config = load_config(config_file)
+        bitstream_file = config.get(KEY_BITSTREAM_FILE)
+        fpga_sclk = config.get(KEY_FPGA_SCLK)
+        fpga_sdata = config.get(KEY_FPGA_SDATA)
+
+    except (KeyError, ValueError) as e:
+        print(f"Configuration error: {e}")
+        return
 
     board = Board(
-        fpga_sclk=17,
-        fpga_sdata=18,
+        fpga_sclk=fpga_sclk,
+        fpga_sdata=fpga_sdata,
     )
-
-    bitstream_file = config.get("bitstream_file")
-
-    if not bitstream_file:
-        print("Missing 'bitstream_file' in config.")
-        return
 
     board.transmit_bitstream(bitstream_file)
     board.disable_bitbang()
